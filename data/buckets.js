cbe66e3225a175f5949dbb7a0ee424fdad3fe3ac
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-19 13:43:12 -0800

    Initial product data setup

---
+++ data/buckets.js
@@ -0,0 +1,64 @@
+const wailPail = {
+    id: 1,
+    name: 'The Wail Pail',
+    image: '',
+    description: 'placeholder description',
+    category: 'self-help',
+    price: 0
+};
+
+const mailPail = {
+    id: 2,
+    name: 'The Mail Pail',
+    image: '',
+    description: 'placeholder description',
+    category: 'logistics',
+    price: 0
+};
+
+const snailPail = {
+    id: 3,
+    name: 'The Snail Pail',
+    image: '',
+    description: 'placeholder description',
+    category: 'culinary',
+    price: 0
+};
+
+const rucket = {
+    id: 4,
+    name: 'The Rucket',
+    image: '',
+    description: 'placeholder description',
+    category: 'utility',
+    price: 0
+};
+
+const mucket = {
+    id: 5,
+    name: 'The Mucket',
+    image: '',
+    description: 'placeholder description',
+    category: 'agricultural',
+    price: 0
+};
+
+const trucket = {
+    id: 6,
+    name: 'The Trucket',
+    image: '',
+    description: 'placeholder description',
+    category: 'oversized',
+    price: 0
+};
+
+const buckets = [
+    wailPail,
+    mailPail,
+    snailPail,
+    rucket,
+    mucket,
+    trucket
+];
+
+export default buckets;
