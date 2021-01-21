53aea73c3e67d3277db8a53add1727afb596aa6d
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-21 15:32:43 -0800

    Add passing test for clearCart

--- test/cart-api.test.js
+++ test/cart-api.test.js
@@ -43,17 +43,33 @@ test('It should correctly set the cart in localStorage', (expect) => {
     setCart(testCart);
 	
     const expected = JSON.stringify(testCart);
-	
     const actual = localStorage.getItem('CART');
 
     expect.equal(actual, expected);
 });
 
-// // test clearCart
-// test('', (expect) => {
-//     const expected = 
-    
-//     const actual = 
+// test clearCart
+test('It should replace the current cart in localStorage with an empty cart', (expect) => {
+	// setting up a placeholder cart to ensure it has something to clear
+    const testCart = [
+        {
+            id: 2,
+            quantity: 1
+        },
+        {
+            id: 5,
+            quantity: 3
+        }
+    ];
 
-//     expect.equal(actual, expected);
-// });
+    const stringyCart = JSON.stringify(testCart);
+    localStorage.setItem('CART', stringyCart);
+	
+	// running clearCart
+    clearCart();
+
+    const expected = JSON.stringify([]);
+    const actual = localStorage.getItem('CART');
+
+    expect.equal(actual, expected);
+});
