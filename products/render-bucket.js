d861da900a461bb1dd94ef3ecf2441f64b233393
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-21 10:05:18 -0800

    Add basic event listener to renderBucket function

--- products/render-bucket.js
+++ products/render-bucket.js
@@ -4,28 +4,28 @@ export function renderBucket(bucket) {
 
     const h3 = document.createElement('h3');
     h3.textContent = bucket.name;
-    li.append(h3);
 
     const img = document.createElement('img');
     img.src = `../assets/${bucket.image}`;
-    li.append(img);
 
     const pDescription = document.createElement('p');
     pDescription.classList.add('description');
     pDescription.textContent = bucket.description;
-    li.append(pDescription);
 
     const pPrice = document.createElement('p');
     pPrice.classList.add('price');
     pPrice.textContent = `$${bucket.price}`;
-    li.append(pPrice);
 
     const addButton = document.createElement('button');
     addButton.classList.add('add-button');
     addButton.value = bucket.id;
     addButton.textContent = 'Add to cart';
-    li.append(addButton);
 
+    addButton.addEventListener('click', () => {
+        console.log(`someone added ${bucket.name}`);
+    });
+
+    li.append(h3, img, pDescription, pPrice, addButton);
     return li;
 
 }
