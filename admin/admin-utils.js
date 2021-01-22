0422286114adbde158ba6f96439efc51b7fad89d
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-22 15:36:41 -0800

    Move form submission into dedicated function

--- admin/admin-utils.js
+++ admin/admin-utils.js
@@ -8,6 +8,19 @@ export function addProduct(product) {
     localStorage.setItem(PRODUCTS, JSON.stringify(products));
 }
 
-export function submitProductForm() {
+export function submitProductForm(form) {
+    const productFormData = new FormData(form);
+
+    const product = {
+        id: productFormData.get('id'),
+        name: productFormData.get('name'),
+        image: productFormData.get('image'),
+        description: productFormData.get('description'),
+        category: productFormData.get('category'),
+        price: productFormData.get('price')
+    };
 	
+    console.log(product); // success
+	
+    addProduct(product);
 }
--- admin/admin.js
+++ admin/admin.js
@@ -1,24 +1,7 @@
-import { addProduct } from './admin-utils.js';
+import { submitProductForm } from './admin-utils.js';
 const productForm = document.getElementById('add-new-bucket');
-
-console.log(productForm); // success
 
 productForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log('Someone clicked the form'); // success
-	
-    const productFormData = new FormData(productForm);
-
-    const product = {
-        id: productFormData.get('id'),
-        name: productFormData.get('name'),
-        image: productFormData.get('image'),
-        description: productFormData.get('description'),
-        category: productFormData.get('category'),
-        price: productFormData.get('price')
-    };
-	
-    console.log(product); // success
-	
-    addProduct(product);
+    submitProductForm(productForm);
 });
