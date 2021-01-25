efb457b69380487310677005a37157ea176a5488
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-24 18:52:24 -0800

    Initial setup for product display and editing on admin page

--- admin/admin-utils.js
+++ admin/admin-utils.js
@@ -28,58 +28,24 @@ export function submitProductForm(form) {
 
 
 // need to adapt to admin page needs
-export function renderProductInList(bucket) {
+export function renderProductInList(product) {
     const li = document.createElement('li');
     li.classList.add('product-entry');
 
-    const h3 = document.createElement('h3');
-    h3.textContent = bucket.name;
+    const h4 = document.createElement('h4');
+    h4.textContent = product.name;
 
     const img = document.createElement('img');
-    img.src = `../assets/${bucket.image}`;
+    img.src = `../assets/${product.image}`;
 
-    const pDescription = document.createElement('p');
-    pDescription.classList.add('description');
-    pDescription.textContent = bucket.description;
+    const removeButton = document.createElement('button');
+    removeButton.textContent = '-';
 
-    const pPrice = document.createElement('p');
-    pPrice.classList.add('price');
-    pPrice.textContent = `$${bucket.price}`;
-    
-    const addItemDiv = document.createElement('div');
-    addItemDiv.classList.add('add-items');
-
-    const quantityInput = document.createElement('input');
-    quantityInput.type = 'number';
-    quantityInput.value = '1';
-    quantityInput.classList.add('add-quantity');
-
-    const addButton = document.createElement('button');
-    addButton.classList.add('add-button');
-    addButton.value = bucket.id;
-    addButton.textContent = 'Add to cart';
-
-    addItemDiv.append(quantityInput, addButton);
-
-    const pQuantity = document.createElement('p');
-    pQuantity.classList.add('quantity');
-
-    addButton.addEventListener('click', () => {
-        const addQuantity = quantityInput.valueAsNumber;
-        addToCart(bucket.id, addQuantity);
-        renderQuantity(bucket.id, pQuantity);
+    removeButton.addEventListener('click', () => {
+        console.log('someone clicked the remove button');
     });
 
-    li.append(h3, img, pDescription, pPrice, addItemDiv, pQuantity);
+    li.append(h4, img, removeButton);
 
     return li;
-}
-
-// in process
-function removeFromProductsList(array, clickedItem) {
-
-    const indexToDelete = array.indexOf(clickedItem);
-    array.splice(indexToDelete, 1);
-
-    return array;
 }
--- admin/admin.js
+++ admin/admin.js
@@ -1,8 +1,16 @@
-import { submitProductForm } from './admin-utils.js';
+import { seedAndGetProducts } from '../products/product-storage.js';
+import { renderProductInList, submitProductForm } from './admin-utils.js';
 
 const productForm = document.getElementById('add-new-bucket');
+const productList = document.getElementById('admin-products');
+const products = seedAndGetProducts();
 
 productForm.addEventListener('submit', (event) => {
     event.preventDefault();
     submitProductForm(productForm);
 });
+
+for (let product of products) {
+    const productEntry = renderProductInList(product);
+    productList.append(productEntry);
+}
